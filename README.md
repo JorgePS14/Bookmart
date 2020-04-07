e17abfa94e9716cfa483e12210990c520afeadd8
Oscar Sierra <oscar_sierra_02@hotmail.com>
2020-04-06 20:24:51 -0600

    modified the folder structure

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Bookmart-back
 
+## Initial configuration
+1. Create a python virtual environment
+2. Get into your virtual environment shell
+3. Go to the root folder
+4. Execute the following commands:
+
+        pipenv install
+
+
 ## Get the server running
-
-type
-
 ```
-flask run
+python3 run.py
 ```
 
 
@@ -13,6 +19,9 @@ flask run
 
 ```
 flask
-pip3
+pipenv
 
 ```
+
+Note: Please, before creating a PR, make sure that your code uses the PEP8 conventions
+to improve your code, please use Flake8 as your linter
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+db = SQLAlchemy()
+from .routes import user_blueprint
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
-    app.config.from_pyfile('settings.py')
-
+    app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_DATABASE_URI'] = '%s://%s:%s@%s/%s' % (
         app.config.get("DB_CONNECTION"),
         app.config.get("DB_USERNAME"),
@@ -14,51 +17,17 @@ def create_app():
         app.config.get("DB_HOST"),
         app.config.get("DB_NAME"),
     )
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    @app.route('/')
-    def index():
-        return "<h1>Back end</h1>"
+    # Initialize plugins
+    db.init_app(app)
 
+    with app.app_context():
+        from app.models import user, book, listing
 
-    CORS(app)
-    @app.route("/api/user", methods=["POST", "GET"])
-    def updateUser():
-        # POST request
-        if request.method == 'POST':
-            user_data = request.get_json()
-            
-            new_user = user.User(
-                user_data["email"],
-                user_data["password"],
-                user_data["username"],
-                user_data["location"],
-                user_data["university"],
-                user_data["semester"],
-                user_data["major"]
-            )
+        db.create_all()
+        db.session.commit()
 
-            db.session.add(new_user)
-            db.session.commit()
-                
-            return 'OK', 200
-        
-        # GET request
-        else:
-            message = {'greeting':'Hello from flask'}
-            return jsonify(message)
+        app.register_blueprint(user_blueprint)
 
-    return app
-
-
-app = create_app()
-db = SQLAlchemy(app)
-
-from app.models import user, book, listing
-
-db.create_all()
-db.session.commit()
-
-if __name__ == "__main__":
-    app.run()
+        return app
