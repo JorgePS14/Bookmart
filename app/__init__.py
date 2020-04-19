d6909aa609c8563b240a1b0e49487406c9542283
Jorge Pietra Santa <31036932+JorgePS14@users.noreply.github.com>
2020-04-18 19:54:13 -0700

    Fixed conflicting imports

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 db = SQLAlchemy()
 from .routes import user_blueprint, book_blueprint, listing_blueprint, request_blueprint
-from .routes import user_blueprint, book_blueprint, listing_blueprint
 
 def create_app():
     app = Flask(__name__)
